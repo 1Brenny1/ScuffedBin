85b47d3243b25d3662b3e661b16b09cf26a029af
1Brenny1 <Brendanrcg@gmail.com>
2023-03-02 12:43:14 -0500

    Removed Note in Cookies.js

--- Cookies.js
+++ Cookies.js
@@ -19,17 +19,3 @@ function setCookie(cname, cvalue, exdays) {
     }
     return "";
   }
-  
-/*
-  function checkCookie() {
-    let user = getCookie("username");
-    if (user != "") {
-      alert("Welcome again " + user);
-    } else {
-      user = prompt("Please enter your name:", "");
-      if (user != "" && user != null) {
-        setCookie("username", user, 365);
-      }
-    }
-  }
-*/
